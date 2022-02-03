566b8a52e92c7643bae74f858ec8924bc331e6c6
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2022-02-03 16:07:09 +0530

    Landing Page - Basic Setup

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,9 @@
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
+import React from "react";
+
+const Landing = () => {
+  return <h4>Landing Page</h4>;
+};
+
+export default Landing;
