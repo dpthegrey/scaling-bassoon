0e3896dd90212fba1c863d1d0f1c8a8adf483a86
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2022-02-03 16:14:10 +0530

    Landing Page - Structure

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -3,7 +3,30 @@ import main from "../assets/images/main.svg";
 import React from "react";
 
 const Landing = () => {
-  return <h4>Landing Page</h4>;
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt="jobify logo" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          {/* info */}
+          <h1>
+            Job <span>tracking</span> app
+          </h1>
+          <p>
+            Drinking vinegar echo park direct trade austin, snackwave tofu
+            humblebrag kogi plaid tbh. Pitchfork ugh cornhole banjo,
+            lumbersexual la croix distillery gastropub fanny pack kogi. Neutra
+            tofu hammock church-key you probably haven't heard of them PBR&B
+            beard.
+          </p>
+          <button className="btn btn-hero">Get started</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </main>
+  );
 };
 
 export default Landing;
