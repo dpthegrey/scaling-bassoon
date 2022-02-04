08ac1ebedf933a4b8ef58eacaeeb05fafb356ebf
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2022-02-04 20:21:01 +0530

    Connect to DB

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
-import dotenv from "dotenv";
 import express from "express";
-import errorHandlerMiddleware from "./middleware/error-handler.js";
-import notFoundMiddleware from "./middleware/not-found.js";
 const app = express();
-
+import dotenv from "dotenv";
 dotenv.config();
 
+import connectDB from "./db/connect.js";
+
+// middelware
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+import notFoundMiddleware from "./middleware/not-found.js";
+
 app.get("/", (req, res) => {
-  throw new Error("Something went wrong when hitting the server");
   res.send("Hello World");
 });
 
-// middelware
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`server is running on port ${port}`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
