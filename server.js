a45b75c8b85a348ea71fc5b4d49b82907693f9b0
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2022-02-04 18:25:30 +0530

    Page not found Middleware

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import express from "express";
+import notFoundMiddleware from "./middleware/not-found.js";
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+// middelware
+app.use(notFoundMiddleware);
 
 const port = process.env.PORT || 5000;
 
