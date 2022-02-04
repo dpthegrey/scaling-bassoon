86434ba123ebc4b3d8de50e2898456767703b677
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2022-02-04 19:09:26 +0530

    ErrorHandlerMiddleware

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 import express from "express";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
 import notFoundMiddleware from "./middleware/not-found.js";
 const app = express();
 
 app.get("/", (req, res) => {
+  throw new Error("Something went wrong when hitting the server"); 
   res.send("Hello World");
 });
 
 // middelware
 app.use(notFoundMiddleware);
-
+app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
