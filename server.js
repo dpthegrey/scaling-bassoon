20eda9bcd71b83382f0aed55296ab46d70a1ed53
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2022-02-04 18:14:35 +0530

    Basic Express Server and Nodemon

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("Server running...");
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
