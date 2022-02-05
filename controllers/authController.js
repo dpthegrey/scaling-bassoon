98dc2b3cbf37b37b44864a215da2a73bc413b9c7
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2022-02-05 19:31:45 +0530

    Check For Duplicate Email in Controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,23 +1,26 @@
-import User from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
 
 import { BadRequestError } from "../errors/index.js";
+import User from "../models/User.js";
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
-
   if (!name || !email || !password) {
     throw new BadRequestError("Please provide all required fields");
   }
-
+  const userAlreadyExists = await User.findOne({ email });
+  if (userAlreadyExists) {
+    throw new BadRequestError("Email already in use");
+  }
   const user = await User.create({ name, email, password });
   res.status(StatusCodes.CREATED).json({ user });
-
   res.send("register user");
 };
+
 const login = async (req, res) => {
   res.send("login user");
 };
+
 const updateUser = async (req, res) => {
   res.send("updateUser");
 };
