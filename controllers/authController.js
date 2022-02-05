37671146172e03df69894988f1c9e6f8cedbca05
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2022-02-05 18:30:26 +0530

    BadRequestError and NotFoundError

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,23 @@
 import User from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
 
-class CustomAPIError extends Error{
-  constructor(message){
-    super(message)
-    this.statusCode = StatusCodes.BAD_REQUEST
+class CustomAPIError extends Error {
+  constructor(message) {
+    super(message);
+  }
+}
+
+class BadRequestError extends CustomAPIError {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
+
+class NotFoundError extends CustomAPIError {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.NOT_FOUND;
   }
 }
 
@@ -12,7 +25,7 @@ const register = async (req, res) => {
   const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
-    throw new CustomAPIError("Please provide all required fields");
+    throw new BadRequestError("Please provide all required fields");
   }
 
   const user = await User.create({ name, email, password });
