d46f231716602bd70ae9096c74ca107396b2a020
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2022-02-05 14:00:07 +0530

    Setup express-async-errors package

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,9 @@
 import User from "../models/User.js";
 
 const register = async (req, res) => {
-  try {
-    const user = await User.create(req.body);
-    res.status(201).json({ user });
-  } catch (error) {
-    next(error);
-  }
+  const user = await User.create(req.body);
+  res.status(201).json({ user });
+
   res.send("register user");
 };
 const login = async (req, res) => {
