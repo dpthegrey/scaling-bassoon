1b5309853a962455ab8ec546fe47a0234e816bff
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2022-02-05 15:57:37 +0530

    Missing Field Error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -6,7 +6,15 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
     message: "Internal Server Error",
   };
-  res.status(defaultError.statusCode).json({ msg: err });
+  if (err.name === "ValidationError") {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    // defaultError.msg = err.message;
+    defaultError.msg = Object.values(err.errors)
+      .map((error) => error.message)
+      .join(", ");
+  }
+  // res.status(defaultError.statusCode).json({ msg: err });
+  res.status(defaultError.statusCode).json({ msg: defaultError.msg });
 };
 
 export default errorHandlerMiddleware;
