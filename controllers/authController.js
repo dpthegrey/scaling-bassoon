0f0c6ebc00582d236fc4f7a5cd8bdf529b3259b5
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2022-02-08 21:39:56 +0530

    Login Controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 import { StatusCodes } from "http-status-codes";
 
-import { BadRequestError } from "../errors/index.js";
+import { BadRequestError, UnAuthenticatedError } from "../errors/index.js";
 import User from "../models/User.js";
 
 const register = async (req, res) => {
@@ -13,22 +13,38 @@ const register = async (req, res) => {
     throw new BadRequestError("Email already in use");
   }
   const user = await User.create({ name, email, password });
+
   const token = user.createJWT();
-  res
-    .status(StatusCodes.CREATED)
-    .json({
-      user: {
-        email: user.email,
-        lastName: user.lastName,
-        location: user.location,
-        name: user.name,
-      },
-      token,
-    });
+  res.status(StatusCodes.CREATED).json({
+    user: {
+      email: user.email,
+      lastName: user.lastName,
+      location: user.location,
+      name: user.name,
+    },
+    token,
+    location: user.location,
+  });
 };
 
 const login = async (req, res) => {
-  res.send("login user");
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Please provide all required fields");
+  }
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    throw new UnAuthenticatedError("Invalid email or password");
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnAuthenticatedError("Invalid email or password");
+  }
+  const token = user.createJWT();
+  user.password = undefined;
+  res.status(StatusCodes.OK).json({ user, token, location: user.location });
 };
 
 const updateUser = async (req, res) => {
