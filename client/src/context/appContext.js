7087aedcba0a6aed27c17f87449cbef9b9bdb415
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2022-02-08 13:10:35 +0530

    Persist User In Local Storage

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -8,15 +8,19 @@ import {
   REGISTER_USER_ERROR,
 } from "./actions";
 
+const token = localStorage.getItem("token");
+const user = localStorage.getItem("user");
+const userLocation = localStorage.getItem("location");
+
 const initialState = {
   isLoading: false,
   showAlert: false,
   alertText: "",
   alertType: "",
-  user: null,
-  token: null,
-  userLocation: "",
-  jobLocation: "",
+  user: user ? JSON.parse(user) : null,
+  token: token,
+  userLocation: userLocation || "",
+  jobLocation: userLocation || "",
 };
 
 const AppContext = React.createContext();
@@ -35,6 +39,18 @@ const AppProvider = ({ children }) => {
     }, 3000);
   };
 
+  const addUserToLocalStorage = ({ user, token, location }) => {
+    localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("token", token);
+    localStorage.setItem("location", location);
+  };
+
+  const removeUserFromLocalStorage = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    localStorage.removeItem("location");
+  };
+
   const registerUser = async (currentUser) => {
     dispatch({ type: REGISTER_USER_BEGIN });
     try {
@@ -45,7 +61,7 @@ const AppProvider = ({ children }) => {
         type: "REGISTER_USER_SUCCESS",
         payload: { user, token, location },
       });
-      // local storage later
+      addUserToLocalStorage({ user, token, location });
     } catch (error) {
       // console.log(error.response);
       dispatch({
