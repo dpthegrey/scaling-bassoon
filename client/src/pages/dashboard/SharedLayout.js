f255bcf713a6ef84789aae4e244541f514e4bb66
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2022-02-09 15:30:00 +0530

    Dashboard Pages - Complete

--- client/src/pages/dashboard/SharedLayout.js
+++ client/src/pages/dashboard/SharedLayout.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Outlet, Link } from "react-router-dom";
+import Wrapper from "../../assets/wrappers/SharedLayout";
 
 const SharedLayout = () => {
-  return <div>shared layout</div>;
+  return (
+    <Wrapper>
+      <nav>
+        <Link to="add-job">add job</Link>
+        <Link to="all-job">all job</Link>
+      </nav>
+      <Outlet />
+    </Wrapper>
+  );
 };
 
 export default SharedLayout;
