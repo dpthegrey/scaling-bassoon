46bd893bd2378eaa1061bc7f7ab3e21261c921f3
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2022-02-15 11:52:34 +0530

    Search Context Setup

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -62,6 +62,11 @@ const initialState = {
   page: 1,
   stats: {},
   monthlyApplications: [],
+  search: "",
+  searchStatus: "all",
+  searchType: "all",
+  sort: "latest",
+  sortOptions: ["latest", "oldest", "a-z", "z-a"],
 };
 
 const AppContext = React.createContext();
@@ -336,6 +341,10 @@ const AppProvider = ({ children }) => {
     clearAlert();
   };
 
+  const clearFilters = () => {
+    console.log("clear filters");
+  };
+
   return (
     <AppContext.Provider
       value={{
@@ -355,6 +364,7 @@ const AppProvider = ({ children }) => {
         deleteJob,
         editJob,
         showStats,
+        clearFilters,
       }}
     >
       {children}
